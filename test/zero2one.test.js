c652ba9adeca51f79ab220b7cd9ba34c5afcb6c7
wolfgang <wolfgang@lemon3.at>
2024-04-15 12:14:29 +0200

    Create zero2one.test.js

---
+++ test/zero2one.test.js
@@ -0,0 +1,15 @@
+import { describe, test, expect } from 'vitest';
+import Zero2one from '../src/zero2one.js';
+
+describe('Zero2one Class tests', () => {
+  test('Zero2one is Object', () => {
+    expect(Zero2one).toBeTruthy();
+    expect(typeof Zero2one).toBe('function');
+  });
+
+  test('new Zero2one() is Object', () => {
+    const bp = new Zero2one();
+    expect(bp).toBeTruthy();
+    expect(typeof bp).toBe('object');
+  });
+});
